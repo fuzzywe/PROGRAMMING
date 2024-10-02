597514b473d541c2f1a61743060cfdc69f6aacaf
JANANI ASOKAN <chicatechhub@gmail.com>
2024-10-02 09:11:18 +0530

    Create SYSTEM.java

---
+++ JAVA/GENERAL/SYSTEM.java
@@ -0,0 +1,6 @@
+The System class is located in the java.lang package, which is loaded by the Java Virtual Machine (JVM) at runtime.
+The System class in Java is defined in the java.lang package, which is part of the core Java libraries and is automatically imported into every Java program.
+
+Key Points about the System Class:
+It is a final class, meaning it cannot be subclassed.
+The class provides several useful class fields and methods that allow you to perform basic system-related functions like input and output, accessing system properties, and interacting with the JVM.
