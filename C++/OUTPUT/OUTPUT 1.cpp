05b6f2e21c87ad1a99d736725be89601bf81e02b
JANANI ASOKAN <chicatechhub@gmail.com>
2024-10-02 15:36:30 +0530

    Create OUTPUT 1.cpp

---
+++ C++/OUTPUT/OUTPUT 1.cpp
@@ -0,0 +1,64 @@
+1.What will be the output of the following C++ code?
+
+#include <iostream> 
+#include <string>
+using namespace std; 
+int main(int argc, char const *argv[])
+{
+	char s1[6] = "Hello";
+	char s2[6] = "World";
+	char s3[12] = s1 + " " + s2;
+	cout<<s3;
+	return 0;
+}
+
+Explanation:
+C-style strings (char[]) do not support the + operator for concatenation. In C++, concatenation of char[] requires the use of functions such as strcat from the C Standard Library (<cstring>) or conversion to std::string (which does support +).
+char s3[12] = s1 + " " + s2; is attempting to add together char[] arrays and a string literal (" "), which is invalid.
+Correct Approach:
+To fix the issue, you can either use the strcat function or convert the C-style strings to std::string for concatenation.
+
+Fixed Code Using std::string:
+cpp
+Copy code
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string s1 = "Hello";
+    string s2 = "World";
+    string s3 = s1 + " " + s2;  // Concatenating using std::string
+    cout << s3;
+    return 0;
+}
+Output:
+Copy code
+Hello World
+Fixed Code Using strcat:
+If you need to use char[], you can use strcat:
+
+cpp
+Copy code
+#include <iostream>
+#include <cstring>  // For strcat
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    char s1[6] = "Hello";
+    char s2[6] = "World";
+    char s3[12] = "";  // Make sure s3 is large enough to hold the concatenated result
+    strcat(s3, s1);    // First copy s1 into s3
+    strcat(s3, " ");   // Then concatenate a space
+    strcat(s3, s2);    // Finally, concatenate s2
+    cout << s3;
+    return 0;
+}
+Output:
+Copy code
+Hello World
+In this version, we use strcat to concatenate the C-style strings.
+
+  
